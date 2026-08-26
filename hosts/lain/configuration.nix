dab188be4c888e436078a132de209e8124b63af8
Matheus Tregnago <maugustotreg@gmail.com>
2026-08-26 14:37:15 -0300

    fix: lain docker

--- hosts/lain/configuration.nix
+++ hosts/lain/configuration.nix
@@ -122,12 +122,16 @@
 
   virtualisation = {
     containers.enable = true;
+    containers.registries.search = [
+      "docker.io"
+    ];
     podman = {
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
     };
   };
+
 
   services.postgresql = {
     enable = true;
